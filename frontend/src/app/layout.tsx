36b4f07d3ab3a2b68a6b50bc32ba52e972636653
peterlidee <peter@lidee.be>
2024-03-07 17:15:19 +0100

    Added provider

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,24 +3,32 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import NavBar from '@/components/header/Navbar';
 
+// import { SessionProvider } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
+import { authOptions } from './api/auth/[...nextauth]/authOptions';
+import Provider from '@/components/Provider';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Frontend',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
   return (
     <html lang='en'>
       <body className={`${inter.className}  px-2 bg-zinc-200`}>
-        <div className='max-w-6xl mx-auto'>
-          <NavBar />
-          <main className='my-4'>{children}</main>
-        </div>
+        <Provider session={session}>
+          <div className='max-w-6xl mx-auto'>
+            <NavBar />
+            <main className='my-4'>{children}</main>
+          </div>
+        </Provider>
       </body>
     </html>
   );
