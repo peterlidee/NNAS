f83b18d45c87dccb2fcc1a0bbc70a68f97358172
peterlidee <peter@lidee.be>
2024-03-09 21:10:53 +0100

    Added small comment.

--- frontend/src/hooks/useCallbackUrl.ts
+++ frontend/src/hooks/useCallbackUrl.ts
@@ -3,6 +3,7 @@ import { usePathname, useSearchParams } from 'next/navigation';
 export default function useCallbackUrl() {
   const pathname = usePathname();
   const params = useSearchParams().toString();
+  // if there are no params, don't add the ?
   const callbackUrl = `${pathname}${params ? '?' : ''}${params}`;
   return callbackUrl;
 }
