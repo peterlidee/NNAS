c9fc0705573311781b92e4082f514d737270d39c
peterlidee <peter@lidee.be>
2024-03-09 12:28:21 +0100

    Added callbackUrl in options

--- frontend/src/components/auth/signIn/GoogleSignInButton.tsx
+++ frontend/src/components/auth/signIn/GoogleSignInButton.tsx
@@ -1,12 +1,15 @@
 'use client';
 
 import { signIn } from 'next-auth/react';
+import { useSearchParams } from 'next/navigation';
 
 export default function GoogleSignInButton() {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
   return (
     <button
       className='bg-white border border-zinc-300 py-1 rounded-md w-full text-zinc-700'
-      onClick={() => signIn('google')}
+      onClick={() => signIn('google', { callbackUrl: callbackUrl })}
     >
       <span className='text-red-700 mr-2'>G</span> Sign in with Google
     </button>
