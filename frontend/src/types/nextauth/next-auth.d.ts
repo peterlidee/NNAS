45bb3064c8332869c560a9838a6774ce4869a6bf
peterlidee <peter@lidee.be>
2024-03-11 21:44:52 +0100

    Extended nextauth interfaces

---
+++ frontend/src/types/nextauth/next-auth.d.ts
@@ -0,0 +1,36 @@
+// https://next-auth.js.org/getting-started/typescript
+// not sure about any of this but it throws no TS errors (anymore)
+
+import NextAuth, { DefaultSession } from 'next-auth';
+import { JWT } from 'next-auth/jwt';
+import { StrapiUserT } from './strapi/StrapiLogin';
+
+declare module 'next-auth' {
+  // Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+
+  interface Session {
+    strapiToken?: string;
+    provider?: 'google' | 'local';
+    user: User;
+  }
+
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+  interface User extends DefaultSession['user'] {
+    // not setting this will throw ts error in authorize function
+    strapiUserId?: number;
+    blocked?: boolean;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  // Returned by the `jwt` callback and `getToken`, when using JWT sessions
+  interface JWT {
+    strapiUserId?: number;
+    blocked?: boolean;
+    strapiToken?: string;
+    provider?: 'local' | 'google';
+  }
+}
