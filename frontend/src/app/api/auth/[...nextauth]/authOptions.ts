24cb60e6ad1e1e63edb643e264066eca6fc287db
peterlidee <peter@lidee.be>
2024-03-17 18:02:49 +0100

    Updated authorize function

--- frontend/src/app/api/auth/[...nextauth]/authOptions.ts
+++ frontend/src/app/api/auth/[...nextauth]/authOptions.ts
@@ -20,29 +20,50 @@ export const authOptions: NextAuthOptions = {
         password: { label: 'Password *', type: 'password' },
       },
       async authorize(credentials, req) {
-        const strapiResponse = await fetch(
-          `${process.env.STRAPI_BACKEND_URL}/api/auth/local`,
-          {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json',
-            },
-            body: JSON.stringify({
-              identifier: credentials!.identifier,
-              password: credentials!.password,
-            }),
-          }
-        );
+        // make sure the are credentials
+        if (!credentials || !credentials.identifier || !credentials.password) {
+          return null;
+        }
+        try {
+          const strapiResponse = await fetch(
+            `${process.env.STRAPI_BACKEND_URL}/api/auth/local`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-type': 'application/json',
+              },
+              body: JSON.stringify({
+                identifier: credentials!.identifier,
+                password: credentials!.password,
+              }),
+            }
+          );
 
-        const data: StrapiLoginResponseT = await strapiResponse.json();
-        return {
-          name: data.user.username,
-          email: data.user.email,
-          id: data.user.id.toString(),
-          strapiUserId: data.user.id,
-          blocked: data.user.blocked,
-          strapiToken: data.jwt,
-        };
+          if (!strapiResponse.ok) {
+            // return error to signIn callback
+            const contentType = strapiResponse.headers.get('content-type');
+            if (contentType === 'application/json; charset=utf-8') {
+              const data: StrapiErrorT = await strapiResponse.json();
+              throw new Error(data.error.message);
+            } else {
+              throw new Error(strapiResponse.statusText);
+            }
+          }
+
+          // success
+          const data: StrapiLoginResponseT = await strapiResponse.json();
+          return {
+            name: data.user.username,
+            email: data.user.email,
+            id: data.user.id.toString(),
+            strapiUserId: data.user.id,
+            blocked: data.user.blocked,
+            strapiToken: data.jwt,
+          };
+        } catch (error) {
+          // Catch errors in try but also f.e. connection fails
+          throw error;
+        }
       },
     }),
   ],
