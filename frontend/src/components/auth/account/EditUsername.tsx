523974817f95aed3440d5f24b140b3d9d3d2af33
peterlidee <peter@lidee.be>
2024-03-22 21:01:59 +0100

    Added toggle functionality

--- frontend/src/components/auth/account/EditUsername.tsx
+++ frontend/src/components/auth/account/EditUsername.tsx
@@ -1,12 +1,31 @@
+'use client';
+
+import { useState } from 'react';
+
 type Props = {
   username: string;
 };
 
 export default function EditUsername({ username }: Props) {
+  const [edit, setEdit] = useState(false);
   return (
     <div className='mb-2'>
-      <div className='block italic'>Username: </div>
-      <div>{username}</div>
+      <label htmlFor='username' className='block italic'>
+        Username:
+      </label>
+      <div className='flex gap-1'>
+        {!edit && <div>{username}</div>}
+        {edit && 'INPUT + BUTTON'}
+        <button
+          type='button'
+          onClick={() => {
+            setEdit((prev) => !prev);
+          }}
+          className='underline text-sky-700'
+        >
+          {edit ? 'close' : 'edit'}
+        </button>
+      </div>
     </div>
   );
 }
