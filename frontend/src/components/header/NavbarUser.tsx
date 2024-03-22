1352c4b28580b748f517de376e5491d4ed54ba6b
peterlidee <peter@lidee.be>
2024-03-22 17:32:17 +0100

    Added link to /account

--- frontend/src/components/header/NavbarUser.tsx
+++ frontend/src/components/header/NavbarUser.tsx
@@ -2,6 +2,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
 import SignOutButton from './SignOutButton';
 import SignInLink from './SignInLink';
+import Link from 'next/link';
 
 export default async function NavbarUser() {
   const session = await getServerSession(authOptions);
@@ -10,7 +11,9 @@ export default async function NavbarUser() {
   }
   return (
     <>
-      <div className='text-sky-700'>{session.user?.name}</div>
+      <Link href='/account' className='text-sky-700 underline'>
+        {session.user?.name}
+      </Link>
       <SignOutButton />
     </>
   );
