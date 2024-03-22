3b0f6a17658c2a0f06cd23f51f1021488a29ac61
peterlidee <peter@lidee.be>
2024-03-22 17:32:06 +0100

    Updated Account with currentUser data

--- frontend/src/components/auth/account/Account.tsx
+++ frontend/src/components/auth/account/Account.tsx
@@ -1,4 +1,10 @@
+import { authOptions } from '@/app/api/auth/[...nextauth]/authOptions';
+import { getCurrentUser } from '@/lib/fetchData/getCurrentUser';
+import { getServerSession } from 'next-auth';
+
 export default async function Account() {
+  const session = await getServerSession(authOptions);
+  const currentUser = await getCurrentUser(session!.strapiToken!);
   return (
     <div className='bg-zinc-100 rounded-sm px-4 py-8 mb-8'>
       <h2 className='font-bold text-lg mb-4'>Account</h2>
@@ -7,14 +13,16 @@ export default async function Account() {
         <h3 className='font-bold mb-4 text-sky-700'>User Data</h3>
         <div className='mb-2'>
           <div className='block italic'>Username: </div>
-          <div>Bob</div>
+          <div>{currentUser.username}</div>
         </div>
         <div className='mb-2'>
           <div className='block italic'>Email: </div>
-          <div>bob@example.com</div>
+          <div>{currentUser.email}</div>
           <div>(You cannot edit your email.)</div>
         </div>
-        <div className='mb-2'>Last updated: ----</div>
+        <div className='mb-2'>
+          Last updated: {new Date(currentUser.updatedAt).toLocaleString()}
+        </div>
       </div>
 
       <div className='mb-8'>
