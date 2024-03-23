d493c3e52581488310b83c3b268f9a06bec2096a
peterlidee <peter@lidee.be>
2024-03-23 11:10:59 +0100

    Not a label yet!

--- frontend/src/components/auth/account/EditUsername.tsx
+++ frontend/src/components/auth/account/EditUsername.tsx
@@ -10,9 +10,7 @@ export default function EditUsername({ username }: Props) {
   const [edit, setEdit] = useState(false);
   return (
     <div className='mb-2'>
-      <label htmlFor='username' className='block italic'>
-        Username:
-      </label>
+      <div className='block italic'>Username:</div>
       <div className='flex gap-1'>
         {!edit && <div>{username}</div>}
         {edit && 'INPUT + BUTTON'}
